cb4766e0301ed8201facac31e0442f9c5062b3ba
sesnews-35 <mdsnsn135@gmail.com>
2025-08-25 01:07:57 +0600

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,8 @@
 window.SHESH = {
   title: "শেষ নিউজ",
   categories: ["সর্বশেষ","জাতীয়","আন্তর্জাতিক","খেলা","বিনোদন","প্রযুক্তি","অর্থনীতি","রাজনীতি","স্বাস্থ্য","শিক্ষা","ফিচার"],
-  dataPath: "./data/news.json",
-  appsScriptFeed: "https://script.google.com/macros/s/AKfycbxwk3EJXyeD1tlGdo1KBNQEfXPC9DekKaR4JcVXTP2W9BzaAXBkV_iMUmAO1bjex9KwoQ/exec",
+  dataPath: "./data/news.json", // GitHub fallback
+  appsScriptFeed: "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec", // লাইভ ডাটা
   pageSize: 12,
   imageFallback: "./assets/img/placeholder.jpg",
   pollMs: 5000,
